78e67f5d5444a1ed7e7ab9f3187f220c5fcf2c58
Mehedi Hasan <{ID}+{username}@users.noreply.github.com>
2024-10-30 10:28:51 +0600

    modify few things

--- src/pages/Home/components/BestSelling.tsx
+++ src/pages/Home/components/BestSelling.tsx
@@ -7,41 +7,37 @@ import { Link } from "react-router-dom";
 
 const BestSelling = () => {
   const { data = {}, isLoading } = useGetProductsQuery({});
-  const {  products } = data.data|| {}
+  const { products } = data.data || {};
 
   return (
     <section className="bg-white py-5 sm:py-20 max-w-7xl mx-auto px-5 lg:px-0 relative">
-              <div className=" mt-4 flex justify-between items-center ">
-      <h2 className="text-2xl sm:text-3xl font-bold mb-4">
-        Best Selling Products
-      </h2>
+      <div className=" mt-4 flex justify-between items-center ">
+        <h2 className="text-2xl sm:text-3xl font-bold mb-4">
+          Best Selling Products
+        </h2>
         <Link
           to="/products"
           className=" text-white font-bold sm:py-2 px-4 rounded"
         >
-            <Button className="bg-primary hover:bg-black">View More</Button>
-          
+          <Button className="bg-primary hover:bg-black">View More</Button>
         </Link>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-5 py-5">
-        {
-        isLoading ? (
+        {isLoading ? (
           <div className="flex justify-center items-end absolute top-0 right-0 bottom-0 left-0">
             {/* <h1 className="text-3xl font-bold ">Loading...</h1> */}
             <div className="">
-
-            <Loading loading={isLoading}/>
+              <Loading loading={isLoading} />
             </div>
           </div>
         ) : (
-          products?.slice(0,5).map((product:TProduct) => (
-            <Product_Card key={product._id} product={product}/>
-          )) )
-          
-          }
-
-</div>
-
+          products
+            ?.slice(0, 5)
+            .map((product: TProduct) => (
+              <Product_Card key={product._id} product={product} />
+            ))
+        )}
+      </div>
     </section>
   );
 };
--- src/pages/Home/components/Featured.tsx
+++ src/pages/Home/components/Featured.tsx
@@ -10,7 +10,7 @@ const Featured = () => {
   const { products } = data.data || {};
 
   // Function to shuffle an array
-  const shuffleArray = (array:TProduct[]) => {
+  const shuffleArray = (array: TProduct[]) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
@@ -19,27 +19,33 @@ const Featured = () => {
   };
 
   // Shuffle the products array and slice the first 5 elements
-  const randomProducts = products ? shuffleArray([...products]).slice(0, 5) : [];
+  const randomProducts = products
+    ? shuffleArray([...products]).slice(0, 5)
+    : [];
 
   return (
-    <section className="bg-white py-20 max-w-7xl mx-auto px-5 lg:px-0 relative">
-      <div className="text-center mt-4 flex justify-between items-center">
-        <h2 className="text-2xl sm:text-3xl font-bold mb-4">Featured Products</h2>
-        <Link to="/products" className="text-white font-bold sm:py-2 px-4 rounded">
+    <section className="bg-white py-5 sm:py-20 max-w-7xl mx-auto px-5 lg:px-0 relative">
+      <div className=" mt-4 flex justify-between items-center">
+        <h2 className="text-2xl sm:text-3xl font-bold mb-4">
+          Featured Products
+        </h2>
+        <Link
+          to="/products"
+          className="text-white font-bold sm:py-2 px-4 rounded"
+        >
           <Button className="bg-primary hover:bg-black">View More</Button>
         </Link>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-5 py-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-5 py-5 ">
         {isLoading ? (
-           <div className="flex justify-center items-center w-full absolute top-0 right-0 bottom-0 left-0">
-           {/* <h1 className="text-4xl font-semibold"> Loading...</h1> */}
-           <div className="">
-
-           <Loading loading={isLoading}/>
-           </div>
-         </div>
+          <div className="flex justify-center items-end w-full absolute top-3 right-0 bottom-0 left-0 ">
+            {/* <h1 className="text-4xl font-semibold"> Loading...</h1> */}
+            <div className="">
+              <Loading loading={isLoading} />
+            </div>
+          </div>
         ) : (
-          randomProducts?.map((product:TProduct) => (
+          randomProducts?.map((product: TProduct) => (
             <Product_Card key={product._id} product={product} />
           ))
         )}
